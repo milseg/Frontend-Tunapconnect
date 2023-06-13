a615b012cee860d00d3e36ff8c116d0b6b515a7b
Kleber willian Dutra Freire <kleberfreire@MacBook-Air-de-Kleber.local>
2023-06-13 14:22:36 -0300

    criando o create cliente

--- src/pages/service-schedule/create/components/ModalSearchClient/Components/Table/ModalCreateNewClient/index.tsx
+++ src/pages/service-schedule/create/components/ModalSearchClient/Components/Table/ModalCreateNewClient/index.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react'
-import Button from '@mui/material/Button'
-import TextField from '@mui/material/TextField'
+
 import Dialog from '@mui/material/Dialog'
-import DialogActions from '@mui/material/DialogActions'
+
 import DialogContent from '@mui/material/DialogContent'
 import DialogTitle from '@mui/material/DialogTitle'
 import { Controller, useFieldArray, useForm } from 'react-hook-form'
