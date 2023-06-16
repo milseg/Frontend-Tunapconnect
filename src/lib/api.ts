2fe8f89c8cc859d769770489b98079493b2c022b
Kleber willian Dutra Freire <kleberfreire@MacBook-Air-de-Kleber.local>
2023-06-16 10:24:05 -0300

    erro da session resolvido

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -12,7 +12,6 @@ const api = axios.create({
 api.interceptors.request.use(
   async function (config) {
     const session = await getSession()
-    console.log('api')
     const token = session?.user.accessToken
     config.headers.Authorization = token ? ` Bearer ${token}` : ''
 
@@ -24,65 +23,3 @@ api.interceptors.request.use(
 )
 
 export { api }
-
-// export class ApiCore {
-//   constructor() {
-//     // api.interceptors.request.use(
-//     //   async function (config) {
-//     //     const session = await getSession()
-//     //     console.log('api')
-//     //     const token = session?.user.accessToken
-//     //     config.headers.Authorization = token ? ` Bearer ${token}` : ''
-
-//     //     return config
-//     //   },
-//     //   function (error) {
-//     //     return Promise.reject(error)
-//     //   },
-//     // )
-//     console.log('api')
-//   }
-
-//   get = async (url: string, params?: any) => {
-//     console.log('get api')
-//     const session = await getSession()
-//     console.log('session', session)
-//     const token = session?.user.accessToken
-//     let response
-//     if (params) {
-//       const queryString = params
-//         ? Object.keys(params)
-//             .map((key) => key + '=' + params[key])
-//             .join('&')
-//         : ''
-//       response = api.get(`${url}?${queryString}`, {
-//         headers: {
-//           Authorization: token ? ` Bearer ${token}` : '',
-//         },
-//       })
-//     } else {
-//       response = api.get(`${url}`, {
-//         headers: {
-//           Authorization: token ? ` Bearer ${token}` : '',
-//         },
-//       })
-//     }
-//     return response
-//   }
-
-//   delete(data: string) {
-//     return api.delete(data)
-//   }
-
-//   create(url: string, data: any, opts?: any) {
-//     if (opts) {
-//       return api.post(url, data, opts)
-//     }
-
-//     return api.post(url, data)
-//   }
-
-//   update(url: string, data: any) {
-//     return api.put(url, data)
-//   }
-// }
