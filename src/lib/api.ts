87ee0f521c88f792dccb4d1bacf28473328c98f8
Kleber willian Dutra Freire <kleberfreire@MacBook-Air-de-Kleber.local>
2023-07-05 10:13:20 -0300

    resolvendo lint

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,46 +1,46 @@
-import { getSession } from "next-auth/react";
-import axios from "axios";
+import { getSession } from 'next-auth/react'
+import axios from 'axios'
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_APP_API_URL,
-  responseType: "json",
+  responseType: 'json',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
-});
+})
 
 const apiB = axios.create({
   baseURL: process.env.NEXT_PUBLIC_APP_API_URL_B,
-  responseType: "json",
+  responseType: 'json',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
-});
+})
 
 apiB.interceptors.request.use(
   async function (config) {
-    const session = await getSession();
-    const token = session?.user.accessToken;
-    config.headers.Authorization = token ? ` Bearer ${token}` : "";
+    const session = await getSession()
+    const token = session?.user.accessToken
+    config.headers.Authorization = token ? ` Bearer ${token}` : ''
 
-    return config;
+    return config
   },
   function (error) {
-    return Promise.reject(error);
-  }
-);
+    return Promise.reject(error)
+  },
+)
 
 api.interceptors.request.use(
   async function (config) {
-    const session = await getSession();
-    const token = session?.user.accessToken;
-    config.headers.Authorization = token ? ` Bearer ${token}` : "";
+    const session = await getSession()
+    const token = session?.user.accessToken
+    config.headers.Authorization = token ? ` Bearer ${token}` : ''
 
-    return config;
+    return config
   },
   function (error) {
-    return Promise.reject(error);
-  }
-);
+    return Promise.reject(error)
+  },
+)
 
-export { api, apiB };
+export { api, apiB }
