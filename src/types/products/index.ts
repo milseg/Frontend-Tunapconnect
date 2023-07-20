2d4dfb17112e4ea71e10fa5464bcdb6995e5e6d0
tobias57 <tobias@sthorm.io>
2023-07-20 04:17:52 -0300

    feat: create products page and api

---
+++ src/types/products/index.ts
@@ -0,0 +1,10 @@
+export interface IProductsEditDTO {
+  status: number
+  statusText: string
+}
+
+export interface IProductsRequestDTO {
+  current_page: number
+  limit: number
+  total_groups: number
+}
