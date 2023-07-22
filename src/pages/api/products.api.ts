673ab134fc7524bc8912de0dcca4c7a83fbd5af8
tobias57 <tobias@sthorm.io>
2023-07-22 11:54:33 -0300

    fix(api): tunap code param

--- src/pages/api/products.api.ts
+++ src/pages/api/products.api.ts
@@ -2,7 +2,8 @@ import { apiB } from '@/lib/api'
 
 async function getProductsList({ queryKey }: any): Promise<any> {
   const nameKey = queryKey[2] ? `name=${queryKey[2]}` : ''
-  const tunapCode = queryKey[3] ? `tunap_code=${queryKey[4]}` : ''
+  const tunapCode = queryKey[4] ? `tunap_code=${queryKey[4]}` : ''
+  console.log(tunapCode)
   const response = await apiB.get<any>(
     `/products?${nameKey}&current_page=${queryKey[1]}&limit=5&${tunapCode}&company_id=${queryKey[3]}&id_responsible=1`,
   )
