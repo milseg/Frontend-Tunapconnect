e222c0705bf85d6c49516b1b27ae4213c3e7a3d7
tobias57 <tobias@sthorm.io>
2023-07-27 10:23:13 -0300

    fix(product-detail): add id param for products list

--- src/pages/api/products.api.ts
+++ src/pages/api/products.api.ts
@@ -7,7 +7,7 @@ async function getProductsList({
   const nameKey = queryKey[2] ? `name=${queryKey[2]}` : ''
   const tunapCode = queryKey[4] ? `tunap_code=${queryKey[4]}` : ''
   const response = await apiB.get<IProductsRequestDTO>(
-    `/products?${nameKey}&current_page=${queryKey[1]}&limit=5&${tunapCode}&id_responsible=1`,
+    `/products?${nameKey}&current_page=${queryKey[1]}&limit=5&${tunapCode}&company_id=${queryKey[3]}&id_responsible=1`,
   )
   return response.data
 }
